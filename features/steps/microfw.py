6c10b5538560c66fc42618524c7664258f0676cf
Michael Ziegler <michael.ziegler@proemion.com>
2019-12-06 21:17:46 +0100

    validate the order in which rules have to appear

--- features/steps/microfw.py
+++ features/steps/microfw.py
@@ -76,13 +76,28 @@ def step(context):
 
 @then("these rules exist")
 def step(context):
-    for rule in context.text.strip().split("\n"):
+    # We want to find rules in context.rules exactly in the order in which they appear
+    # in context.text. To make this happen, create a generator over context.rules that
+    # we use in the inner for loop, but create it out here so that it does not reset
+    # for each wanted_rule.
+    actual_rule_gen = iter(context.rules)
+
+    for wanted_rule in context.text.strip().split("\n"):
         # replace multiple spaces with a single space to have whitespace changes not matter
-        rule = re.sub(' +', ' ', rule)
-        if not rule:
+        wanted_rule = re.sub(' +', ' ', wanted_rule)
+        if not wanted_rule:
             continue
-        if rule not in context.rules:
-            raise ValueError("Rule is missing: '%s'" % rule)
+        # look for the wanted_rule in actual_rule_gen. If we find it, we're happy and we
+        # continue. If we arrive at the end of the iterator, we raise an error.
+        for actual_rule in actual_rule_gen:
+            if actual_rule == wanted_rule:
+                # Found it!
+                # break so that the "else:" statement below does not execute, which
+                # would raise an exception.
+                # The outer for loop will then either continue, or be done.
+                break
+        else:
+            raise ValueError("Rule is missing: '%s'" % wanted_rule)
 
 @then("these rules do NOT exist")
 def step(context):
