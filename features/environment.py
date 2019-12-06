9abb7778b41368aab2039ef43b0dba227fc84537
Michael Ziegler <michael.ziegler@proemion.com>
2019-12-06 18:43:21 +0100

    docstring as to how to get a /tmp/iptables-rules.txt file

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,11 @@
 import sys
 
 def after_scenario(context, scenario):
+    """
+    If the scenario failed, write its rules to /tmp/iptables-rules.txt for inspection.
+    Protip: If you're developing a test and want to see the current ruleset, match for
+    a rule named "dummy" or "yolo" to provoke an error and get a rule dump.
+    """
     if not sys.stdout.isatty():
         return
 
