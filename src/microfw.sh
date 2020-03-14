380aaa173bccaaa1dfe8404e1e9d0c6d27d0a975
Michael Ziegler <michael.ziegler@proemion.com>
2020-03-14 22:36:42 +0100

    read the state file for tear_down

--- src/microfw.sh
+++ src/microfw.sh
@@ -22,6 +22,7 @@ while [ -n "${1:-}" ]; do
             echo " show          Generate and show setup.sh, but do not write it to disk"
             echo " compile       Update tear_down.sh and setup.sh"
             echo " apply         compile, run setup, prompt for aliveness, run teardown on timeout"
+            echo " tear_down     tear down any existing rules"
             exit 0
             ;;
 
@@ -35,7 +36,7 @@ while [ -n "${1:-}" ]; do
             shift
             ;;
 
-        show|compile|apply)
+        show|compile|apply|tear_down)
             COMMAND="$1"
             ;;
 
@@ -52,16 +53,77 @@ if [ "$COMMAND" = "invalid" ]; then
 fi
 
 
-function generate_tear_down() {
-    echo '#!/bin/bash'
-    echo 'set -e'
-    echo 'set -u'
+function tear_down() {
+    if [ ! -e "$VAR_DIR/state.txt" ]; then
+        # Already torn down -> nothing to do
+        return
+    fi
 
-    echo 'iptables-save  | /usr/local/lib/microfw/generate_teardown.py "iptables"  | /bin/bash'
-    echo 'ip6tables-save | /usr/local/lib/microfw/generate_teardown.py "ip6tables" | /bin/bash'
+    # First let's find out if docker and mangle are present
+    HAVE_DOCKER="$(grep -q "^HAVE-DOCKER$" "$VAR_DIR/state.txt" && echo "true" || echo "false")"
+    HAVE_MANGLE="$(grep -q "^HAVE-MANGLE$" "$VAR_DIR/state.txt" && echo "true" || echo "false")"
 
-    echo ipset flush
-    echo ipset destroy
+    # Little helper function that makes it easier to run the same command for
+    # both iptables and ip6tables
+    function ip+6tables {
+        iptables  "$@"
+        ip6tables "$@"
+    }
+
+    # Now let's actually shut down the firewall
+    # Detach our input chains from the main iptables chains
+    if [ "$HAVE_DOCKER" = "true" ]; then
+        iptables   -t filter -D DOCKER-USER -j MFWFORWARD
+        ip6tables  -t filter -D FORWARD     -j MFWFORWARD
+    else
+        ip+6tables -t filter -D FORWARD     -j MFWFORWARD
+    fi
+    if [ "$HAVE_MANGLE" = "true" ]; then
+        ip+6tables -t mangle -D FORWARD     -j MFWFORWARD
+    fi
+    ip+6tables -t nat    -D PREROUTING  -j MFWPREROUTING
+    ip+6tables -t nat    -D POSTROUTING -j MFWPOSTROUTING
+
+    # Flush chains so the zone-specific chains are free after flush
+    for chain in accept drop reject MFWINPUT MFWFORWARD; do
+        ip+6tables -t filter -F "$chain"
+    done
+    if [ "$HAVE_MANGLE" = "true" ]; then
+        ip+6tables -t mangle -F MFWFORWARD
+    fi
+    for chain in MFWPREROUTING MFWPOSTROUTING; do
+        ip+6tables -t nat    -F "$chain"
+    done
+
+    # Flush and drop zone-specific chains
+    grep "^ZONE " "$VAR_DIR/state.txt" | while read command zone; do
+        ip+6tables -t filter -F "${zone}_inp"
+        ip+6tables -t filter -X "${zone}_inp"
+        ip+6tables -t filter -F "${zone}_fwd"
+        ip+6tables -t filter -X "${zone}_fwd"
+        if [ "$HAVE_MANGLE" = "true" ]; then
+            ip+6tables -t mangle -F "${zone}_fwd"
+            ip+6tables -t mangle -X "${zone}_fwd"
+        fi
+        ;;
+    done
+
+    # The chains that we flushed above, now we drop
+    for chain in accept drop reject MFWINPUT MFWFORWARD; do
+        ip+6tables -t filter -X "$chain"
+    done
+    if [ "$HAVE_MANGLE" = "true" ]; then
+        ip+6tables -t mangle -X MFWFORWARD
+    fi
+    for chain in MFWPREROUTING MFWPOSTROUTING; do
+        ip+6tables -t nat    -X "$chain"
+    done
+
+    # Get rid of ipsets
+    ipset flush
+    ipset destroy
+
+    rm "$VAR_DIR/state.txt"
 }
 
 function generate_setup() {
@@ -74,15 +136,14 @@ function generate_setup() {
 
 function compile() {
     mkdir -p $VAR_DIR
-    generate_tear_down > $VAR_DIR/tear_down.sh
     generate_setup     > $VAR_DIR/setup.sh
-    chmod +x $VAR_DIR/tear_down.sh $VAR_DIR/setup.sh
+    chmod +x $VAR_DIR/setup.sh
 }
 
 function apply() {
     compile
 
-    $VAR_DIR/tear_down.sh
+    tear_down
     $VAR_DIR/setup.sh
 
     echo "REVERTING IN 30 SECONDS, HIT ^c"
@@ -90,7 +151,7 @@ function apply() {
     echo "REVERTING IN 5 SECONDS, HIT ^c"
     sleep 5
 
-    $VAR_DIR/tear_down.sh
+    tear_down
 }
 
 
@@ -103,5 +164,8 @@ case "$COMMAND" in
         ;;
     apply)
         apply
+        ;;
+    tear_down)
+        tear_down
         ;;
 esac
